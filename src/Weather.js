f25b7f4b7d74d03a6e4d9c9d3f0b1b3119eb4bd5
ChiWei-J <angeljan01@gmail.com>
2024-04-26 16:23:47 -0500

    Updated stying of content

--- src/Weather.js
+++ src/Weather.js
@@ -9,8 +9,9 @@ export default function Weather() {
           <div className="col-9">
             <input
               type="search"
-              placeholder="Search for a city"
+              placeholder="Enter a city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
@@ -27,13 +28,19 @@ export default function Weather() {
         <li>Friday 4:00 PM</li>
         <li>Sunny</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-            alt="sunny"
-          />
-          57°F
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="sunny"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">57</span>{" "}
+              <span className="unit">°F</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
