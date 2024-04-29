dc2369ae69c743fe2a949c7c5bc12f9b3ea378eb
ChiWei-J <angeljan01@gmail.com>
2024-04-29 14:29:14 -0500

    installed moment.js to format date and styled form button

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,4 +1,5 @@
 import React from "react";
+import moment from "moment";
 
 export default function FormattedDate(props) {
   let days = [
@@ -11,18 +12,11 @@ export default function FormattedDate(props) {
     "Saturday",
   ];
   let day = days[props.date.getDay()];
-  let hours = props.date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
+  let currentDate = moment().format("MMMM Do YYYY, h:mm a");
 
-  let minutes = props.date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
   return (
     <div>
-      {day} {hours}:{minutes}
+      {day} {currentDate}
     </div>
   );
 }
